2e56a8ed1f5c77e0dc6fbf2115e38b33bd989890
Builders Road <buildersroad2020@gmail.com>
2021-04-16 00:04:55 +1000

    hanges

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
+            @can('admin', App\Models\Users::class)
                 <div>
                     <div class="container mx-auto space-y-4 p-4 sm:p-0 mt-8">
                         <div class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
@@ -19,6 +19,7 @@
                 </div>
 
                 @livewireChartsScripts
+                @endcan
 
                 @can('vendor', App\Models\Users::class)
 
